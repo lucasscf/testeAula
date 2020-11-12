8235ee6209c71d7b519f5725d621966801392ffd
lucasscf <lucasscf@gmail.com>
2020-11-11 21:49:46 -0300

    arquivos projeto inicial

---
+++ src/GenericUtils.java
@@ -0,0 +1,35 @@
+
+public class GenericUtils {
+	                                                     // $1 $2   $3
+	public static String returnTelFormat(String value) { // 12 3456 7890 (10 numeoros)
+		if(value.length() == 10 && isNumeric(value)) {
+			//return "(" + value.substring(0, 2) + ")" + value.substring(2, 6) + "-" + value.substring(7, 10);
+			return String.valueOf(value).replaceFirst("(\\d{2})(\\d{4})(\\d+)", "($1) $2-$3");
+		} else {
+			System.out.println("Valor invalido");
+			return "";
+		}
+	}
+	
+	public static String returnCpfFormat(String value) {
+		if(value.length() == 11 && isNumeric(value)) { 
+			return String.valueOf(value).replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d+)", "$1.$2.$3-$4");
+		} else {
+			System.out.println("Valor invalido");
+			return "";
+		}
+	}
+	
+	public static boolean validEmail(String value) {
+		return (value.contains("@") && value.contains(".com"));
+	}
+	
+	private static boolean isNumeric(String value) {
+		try {
+			Double.parseDouble(value);
+			return true;
+		} catch(NumberFormatException e) {
+			return false;
+		}
+	}
+}
---
+++ src/Programa.java
@@ -0,0 +1,11 @@
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		System.out.println(GenericUtils.returnTelFormat("3197978686"));
+		System.out.println(GenericUtils.returnCpfFormat("01101101111"));
+		System.out.println(GenericUtils.validEmail("lucas@lucas"));
+	}
+
+}
